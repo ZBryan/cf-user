d7ba6b4d0d2a6360d4b518a1b0ac29668007bafc
Zach Bryan <zachx1@gmail.com>
2013-12-22 13:46:12 -0800

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Congrats, you made a new user"
+      redirect_to users_path
+
+    else
+      # flash[:notice] = "Something went horribly wrong.... Please make sure you have a first name list and a valid email adress"
+      render action: 'new'
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+  end
